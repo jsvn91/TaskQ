c1b0cbadd6f74b8d92dc820365d8a10237697fbf
jsvn91 <jsvn91@gmail.com>
2024-02-12 03:32:31 +0530

    updated test

--- taskq/sample.py
+++ taskq/sample.py
@@ -1,9 +1,10 @@
 import logging
-
+import time
 log = logging.getLogger()
 
 
 def sample_target_func(args):
+    time.sleep(2)
     log.info(f"{args}")
     return 1
 
--- test/test_queue.py
+++ test/test_queue.py
@@ -8,7 +8,7 @@ log = logging.getLogger()
 def test_tqueue():
     tq = TQueue(target=sample_target_func, max_parallel_worker=6)
 
-    tq.enqueue_item("HI", "HI", "HI", "HI")
+    tq.enqueue_item("HI", "HI", "HI", "HI","HI", "HI", "HI", "HI", "HI", "HI", "HI", "HI" )
 
     tq.process_queue()
 
